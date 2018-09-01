741bb9a986f54960c9e027362472eb7a069fd6b1
“scottasc” <scottschulfer@gmail.com>
2018-09-01 14:20:46 -0500

    implementing RESTful routes/actions

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,13 +1,40 @@
 class Api::ContactsController < ApplicationController
 
-  def one_contact_action
-    @contact = Contact.last
-    render 'one_contact.json.jbuilder'
+  def index
+    @contacts = Contact.all
+    render 'index.json.jbuilder'
   end
 
-  def many_contacts_action
-    @contacts = Contact.all
-    render 'many_contacts.json.jbuilder'
+  def create
+    @contact = Contact.new(
+                          first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          email: params[:email],
+                          phone_number: params[:phone_number]
+                          )
+    @contact.save
+    render "show.json.jbuilder"
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render json: {message: "Person destroyed!"}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/one_contact_url" => 'contacts#one_contact_action'
-    get "/many_contacts_url" => 'contacts#many_contacts_action'
+    get "/contacts" => 'contacts#index'
+    post "/contacts" => 'contacts#create'
+    get "/contact" => 'contacts#show'
+    patch "/contact/:id" => 'contacts#update'
+    delete "/contact/:id" => 'contacts#destroy'
   end
 end

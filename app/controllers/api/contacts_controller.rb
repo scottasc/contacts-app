3250280f33b998af96fab174bc6a681157f8ca39
“scottasc” <scottschulfer@gmail.com>
2018-09-04 21:30:26 -0500

    classwork 9/4

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -14,8 +14,11 @@ class Api::ContactsController < ApplicationController
                           phone_number: params[:phone_number], 
                           bio: params[:bio]
                           )
-    @contact.save
-    render "show.json.jbuilder"
+    if @contact.save
+      render "show.json.jbuilder"
+    else
+      render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -31,8 +34,12 @@ class Api::ContactsController < ApplicationController
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
     @contact.bio = params[:bio] || @contact.bio
-    @contact.save
-    render "show.json.jbuilder"
+
+    if @contact.save
+      render "show.json.jbuilder"
+    else
+      render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,11 @@
 class Contact < ApplicationRecord
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+
   def friendly_updated_at
     updated_at.strftime("%m/%d/%Y")
   end
